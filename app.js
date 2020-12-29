27fbd12b170f42f90419cc3872cccf913ca8e37a
cristianARG <cristian23villalba@gmail.com>
2020-12-29 10:44:16 -0300

    trabajo par impar

---
+++ app.js
@@ -0,0 +1,6 @@
+function parImpar(numero) {
+    numero % 2 != 0 ? console.log("El numero es impar") : console.log("El numero es par")
+    numero == 0 ? console.log('Error el numero no debe ser "0"') : ""
+
+}
+parImpar(0)
